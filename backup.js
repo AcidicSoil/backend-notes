46b4b74b24e1c49f4e595ff6dfa8c36f8423b875
AcidicSoil <claytonbivens1@gmail.com>
2023-11-14 09:31:12 -0600

    uibuild

--- backup.js
+++ backup.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
 
 const Note = require("./models/note");
 
@@ -13,30 +13,10 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  }
-
-  next(error);
-};
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(requestLogger);
-app.use(express.static("build"));
-
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
@@ -55,6 +35,47 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
 app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
@@ -65,16 +86,14 @@ app.get("/api/notes/:id", (request, response, next) => {
       }
     })
     .catch((error) => next(error));
+  console.log(error);
+  response.status(400).send({ error: "malformatted id" });
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then((result) => {
-      if (result) {
-        response.status(204).end();
-      } else {
-        response.status(404).send({ error: "Note not found" });
-      }
+      response.status(204).end();
     })
     .catch((error) => next(error));
 });
@@ -94,7 +113,22 @@ app.put("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.use(unknownEndpoint);
+app.use((error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+});
+
+app.use((request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+});
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
